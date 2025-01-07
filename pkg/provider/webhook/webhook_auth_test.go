31d5fd3a3a597f1580828c43e0116a8ac07810ef
Yi Fong Au <yi-fong.au@stater.nl>
2025-01-07 16:37:21 +0100

    less code duplication more type safety

--- pkg/provider/webhook/webhook_auth_test.go
+++ pkg/provider/webhook/webhook_auth_test.go
@@ -11,78 +11,145 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type authTestCase struct {
-	AuthMethod string
-	Creds      creds
-	Expected   string
+type mockAuthPackage struct {
+	Request   mockAuthRequest
+	Handler   mockAuthHandler
+	TestCases []mockAuthTestCase
+}
+
+type mockAuthRequest func(url string, testCase mockAuthTestCase, t *testing.T) string
+type mockAuthHandler func(secret string, validCreds creds) http.HandlerFunc
+type mockAuthTestCase struct {
+	Creds    creds
+	Expected string
 }
 
 type creds struct {
-	userName string
-	password string
+	UserName string
+	Password string
 }
 
 func TestWebhookAuth(t *testing.T) {
 
-	// declare dummy data
+	// testing data
 	validCreds := creds{"correctuser", "correctpassword"}
 	invalidCreds := creds{"incorrectuser", "incorrectpassword"}
 	secret := "thisIsTheSecret"
 
-	// declare test cases
-	authTestCases := map[string]authTestCase{
-		"BasicAuth with correct creds": {
-			AuthMethod: "BasicAuth",
-			Creds:      validCreds,
-			Expected:   secret,
-		},
-		"BasicAuth with incorrect creds": {
-			AuthMethod: "BasicAuth",
-			Creds:      invalidCreds,
-			Expected:   "401",
-		},
+	// define test cases
+	authTestCases := []mockAuthTestCase{
+		{validCreds, secret},
+		{invalidCreds, "401"},
 	}
 
-	// create test server with mux
+	// define auth packages
+	mockAuthPackages := map[string]mockAuthPackage{
+		"BasicAuth": {basicAuthRequest, basicAuthHandler, authTestCases},
+		"NTLM":      {ntlmAuthRequest, ntlmAuthHandler, authTestCases},
+	}
+
+	// start test server with mux
 	mux := http.NewServeMux()
-	basicAuthEndpoint := "/BasicAuth"
-	mux.HandleFunc(basicAuthEndpoint, basicAuthHandler(secret, validCreds))
+	for name, mockAuthPackage := range mockAuthPackages {
+		endpoint := "/" + name
+		mux.HandleFunc(endpoint, mockAuthPackage.Handler(secret, validCreds))
+	}
 	authTestServer := httptest.NewServer(mux)
 	defer authTestServer.Close()
 
-	// parse test cases and execute tests
-	for _, testCase := range authTestCases {
-		if testCase.AuthMethod == "BasicAuth" {
-			url := authTestServer.URL + basicAuthEndpoint
-			runAuthTestCase(url, testCase, t)
+	// execute tests
+	for name, mockAuthPackage := range mockAuthPackages {
+		url := authTestServer.URL + "/" + name
+		for _, mockAuthTestCase := range mockAuthPackage.TestCases {
+			result := mockAuthPackage.Request(url, mockAuthTestCase, t)
+			expected := mockAuthTestCase.Expected
+			if result != expected {
+				t.Errorf("got %q, expected %q", result, expected)
+			}
+
 		}
 	}
 }
 
-func runAuthTestCase(url string, testCase authTestCase, t *testing.T) {
-	result := basicAuthRequest(url, testCase, t)
-	expected := testCase.Expected
-
-	if result != expected {
-		t.Errorf("got %q, expected %q", result, expected)
-	}
-}
-
 func basicAuthHandler(secret string, validCreds creds) http.HandlerFunc {
+	// use closure so we can pass the handler the testdata at runtime
 	return func(w http.ResponseWriter, r *http.Request) {
-		validCredsString := validCreds.userName + ":" + validCreds.password
-		receivedCreds, _ := b64.StdEncoding.DecodeString(r.Header.Get("Authorization"))
-		if string(receivedCreds) != validCredsString {
+		validCredsString := b64.StdEncoding.EncodeToString([]byte(validCreds.UserName + ":" + validCreds.Password))
+		receivedCredsString := r.Header.Get("Authorization")
+
+		if receivedCredsString != validCredsString {
 			w.Write([]byte("401"))
-		} else if string(receivedCreds) == validCredsString {
+		} else if receivedCredsString == validCredsString {
 			w.Write([]byte(secret))
 		}
 	}
 }
 
-func basicAuthRequest(url string, testCase authTestCase, t *testing.T) string {
+func ntlmAuthHandler(secret string, validCreds creds) http.HandlerFunc {
+	// use closure so we can pass the handler the testdata at runtime
+	return func(w http.ResponseWriter, r *http.Request) {
+		validCredsString := b64.StdEncoding.EncodeToString([]byte(validCreds.UserName + ":" + validCreds.Password))
+		receivedCredsString := r.Header.Get("Authorization")
+
+		if receivedCredsString != validCredsString {
+			w.Write([]byte("401"))
+		} else if receivedCredsString == validCredsString {
+			w.Write([]byte(secret))
+		}
+	}
+}
+
+func basicAuthRequest(url string, testCase mockAuthTestCase, t *testing.T) string {
 	creds := testCase.Creds
-	credsEnc := b64.StdEncoding.EncodeToString([]byte(creds.userName + ":" + creds.password))
+	credsEnc := b64.StdEncoding.EncodeToString([]byte(creds.UserName + ":" + creds.Password))
+
+	// create ClusterSecretStore
+	testStore := &esv1beta1.ClusterSecretStore{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "ClusterSecretStore",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "webhook-store",
+			Namespace: "default",
+		},
+		Spec: esv1beta1.SecretStoreSpec{
+			Provider: &esv1beta1.SecretStoreProvider{
+				Webhook: &esv1beta1.WebhookProvider{
+					URL: url,
+					Headers: map[string]string{
+						"Authorization": credsEnc,
+					},
+				},
+			},
+		},
+	}
+
+	// create HTTP client from ClusterSecretStore
+	testProv := &Provider{}
+	client, err := testProv.NewClient(context.Background(), testStore, nil, "testnamespace")
+	if err != nil {
+		t.Errorf("Error creating client: \n%q\n%q", testCase, err.Error())
+		return "error"
+	}
+
+	// dummy testRef (unused in this test, but required)
+	testRef := esv1beta1.ExternalSecretDataRemoteRef{
+		Key: "dummy",
+	}
+
+	// perform request, exercising GetSecret
+	resp, err := client.GetSecret(context.Background(), testRef)
+	if err != nil {
+		t.Errorf("Error retrieving secret:\n%s\n%s", testCase, err.Error())
+	}
+
+	return string(resp)
+
+}
+
+func ntlmAuthRequest(url string, testCase mockAuthTestCase, t *testing.T) string {
+	creds := testCase.Creds
+	credsEnc := b64.StdEncoding.EncodeToString([]byte(creds.UserName + ":" + creds.Password))
 
 	// create ClusterSecretStore
 	testStore := &esv1beta1.ClusterSecretStore{
