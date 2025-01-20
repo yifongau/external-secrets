b16e200524ee0f724ccc178e7bf41b5fe2692968
Yi Fong Au <yi-fong.au@stater.nl>
2025-01-21 00:52:27 +0100

    added general exersieGetSecret func

--- pkg/provider/webhook/webhook_auth_test.go
+++ pkg/provider/webhook/webhook_auth_test.go
@@ -3,34 +3,30 @@ package webhook
 import (
 	"context"
 	b64 "encoding/base64"
-	"io"
+	//"io"
 	//"k8s.io/client-go/kubernetes/scheme"
 	"net/http"
 	"net/http/httptest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
-	"strings"
+	//	"strings"
 	"testing"
 
-	"github.com/Azure/go-ntlmssp"
+	//	"github.com/Azure/go-ntlmssp"
 	esv1beta1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1beta1"
 	esmeta "github.com/external-secrets/external-secrets/apis/meta/v1"
-	"github.com/vadimi/go-http-ntlm/v2"
-	"github.com/vadimi/go-ntlm/ntlm"
+	//"github.com/vadimi/go-http-ntlm/v2"
+	//	"github.com/vadimi/go-ntlm/ntlm"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type mockAuthTestPackage struct {
-	ServerCreds   mockCreds
-	ServerSecret  string
-	TestServer    mockAuthTestServer
-	Request       mockAuthRequest
-	LoginAttempts []mockLoginAttempt
-}
-
-type mockLoginAttempt struct {
-	Creds  mockCreds
-	Expect string
+	Creds        mockCreds
+	ServerSecret string
+	TestServer   mockAuthTestServer
+	Request      mockAuthRequest
+	Expect       string
 }
 
 type mockCreds struct {
@@ -51,80 +47,43 @@ type mockAuthRequest func(
 func TestWebhookAuth(t *testing.T) {
 
 	// define test cases
-	validCreds := mockCreds{"correctuser123", "correctpassword123"}
-	//invalidCreds := mockCreds{"incorrectuser123", "incorrectpassword123"}
+	creds := mockCreds{"correctuser123", "correctpassword123"}
+	basicAuthExpect := "Basic " + b64.StdEncoding.EncodeToString([]byte(creds.UserName+":"+creds.Password))
+	ntlmExpect := "NTLM TlRMTVNTUAABAAAAAQCIoAAAAAAoAAAAAAAAACgAAAAGAbEdAAAADw=="
+
 	secret := "secret123"
 
-	loginAttempts := []mockLoginAttempt{
-		{validCreds, secret},
-		//	{invalidCreds, "401"},
-	}
-
 	testPackages := map[string]mockAuthTestPackage{
-		"BasicAuth": {validCreds, secret, basicAuthServer, basicAuthRequest, loginAttempts},
-		"NTLM":      {validCreds, secret, ntlmServer, ntlmRequest, loginAttempts},
+		"BasicAuth": {creds, secret, basicAuthRequestEcho, basicAuthRequest, basicAuthExpect},
+		"NTLM":      {creds, secret, ntlmRequestEcho, ntlmRequest, ntlmExpect},
 	}
 
 	// execute test cases
 	for _, p := range testPackages {
-		server := p.TestServer(p.ServerCreds, p.ServerSecret, t)
+		server := p.TestServer(p.Creds, p.ServerSecret, t)
 		defer server.Close()
 
-		for _, loginAttempt := range p.LoginAttempts {
-			result := p.Request(server.URL, loginAttempt.Creds, t)
-			expect := loginAttempt.Expect
-			if result != expect {
-				t.Errorf("Test failed. Result: '%s' / Expected:  '%s'", result, expect)
-			}
+		result := p.Request(server.URL, creds, t)
+		expect := p.Expect
+		if result != expect {
+			t.Errorf("Test failed. Result: '%s' / Expected:  '%s'", result, expect)
 		}
-
 	}
+
 }
 
-func ntlmServer(creds mockCreds, secret string, t *testing.T) *httptest.Server {
-
-	session, _ := ntlm.CreateServerSession(ntlm.Version2, ntlm.ConnectionlessMode)
-	session.SetUserInfo(creds.UserName, creds.Password, "")
+func ntlmRequestEcho(creds mockCreds, secret string, t *testing.T) *httptest.Server {
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		/*
-			for name, values := range r.Header {
-				t.Log(name, values)
 
-			}*/
-
-		reqAuthHeaderString := r.Header.Get("Authorization")
-		if reqAuthHeaderString == "" {
+		reqAuthString := r.Header.Get("Authorization")
+		if reqAuthString == "" {
 			// go-ntlmssp first sends anonymous request, respond with 401
+			w.Header().Add("WWW-Authenticate", "NTLM")
 			w.WriteHeader(401)
 
 		} else {
-			t.Log("Server received Authorization header:")
-			t.Log(reqAuthHeaderString)
-			ntlmAuthString := strings.Replace(reqAuthHeaderString, "NTLM ", "", -1)
-			authenticateBytes, _ := b64.StdEncoding.DecodeString(ntlmAuthString)
-			t.Log(authenticateBytes)
-			auth, err := ntlm.ParseAuthenticateMessage(authenticateBytes, 2)
-			if err != nil { //  IS NEGOTIATE_MESSAGE, reply with CHALLENGE_MESSAGE
-				challenge, _ := session.GenerateChallengeMessage()
-
-				w.Header().Add("WWW-Authenticate", "NTLM "+b64.StdEncoding.EncodeToString(challenge.Bytes()))
-				w.Header().Add("WWW-Authenticate", `Basic realm="test"`)
-				t.Log("Server sends CHALLENGE_MESSAGE:")
-				t.Log(challenge.ServerChallenge)
-				w.WriteHeader(401)
-
-			} else { // IS AUTHENTICATE_MESSAGE, authenticate
-				t.Log("Received AUTHENTICATE_MESSAGE")
-				err = session.ProcessAuthenticateMessage(auth)
-				if err == nil {
-					w.Write([]byte(secret + "hello"))
-					//	w.WriteHeader((200))
-				} else {
-					w.Write([]byte("401" + "hello"))
-				}
-
-			}
+			w.Write([]byte(reqAuthString))
 		}
 	}))
 	return server
@@ -199,75 +158,15 @@ func ntlmRequest(url string, creds mockCreds, t *testing.T) string {
 	return string(resp)
 }
 
-func ntlmSimpleRequestNew(url string, creds mockCreds, t *testing.T) string {
-
-	client := http.Client{
-		Transport: &httpntlm.NtlmTransport{
-			Domain:       "",
-			User:         creds.UserName,
-			Password:     creds.Password,
-			RoundTripper: &http.Transport{},
-		},
-	}
-
-	req, _ := http.NewRequest("Get", url, nil)
-	res, _ := client.Do(req)
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("Error")
-	}
-	return string(body)
-}
-
-func ntlmSimpleRequestOld(url string, creds mockCreds, t *testing.T) string {
-	/*
-		client := http.Client{
-			Transport: &httpntlm.NtlmTransport{
-				Domain:       "",
-				User:         creds.UserName,
-				Password:     creds.Password,
-				RoundTripper: &http.Transport{},
-			},
-		}
-
-		req, _ := http.NewRequest("Get", url, nil)
-		res, _ := client.Do(req)
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			t.Errorf("Error")
-		}*/
-
-	client := &http.Client{
-		Transport: ntlmssp.Negotiator{
-			RoundTripper: &http.Transport{},
-		},
-	}
-
-	req, _ := http.NewRequest("GET", url, nil)
-	req.SetBasicAuth(creds.UserName, creds.Password)
-	t.Log(req.Header)
-	res, _ := client.Do(req)
-
-	bodyBytes, _ := io.ReadAll(res.Body)
-
-	return string(bodyBytes)
-}
-
-func basicAuthServer(creds mockCreds, secret string, t *testing.T) *httptest.Server {
+func basicAuthRequestEcho(creds mockCreds, secret string, t *testing.T) *httptest.Server {
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		validAuthString := b64.StdEncoding.EncodeToString([]byte(creds.UserName + ":" + creds.Password))
-		reqAuthString := r.Header.Get("Authorization")
 
+		reqAuthString := r.Header.Get("Authorization")
 		if reqAuthString == "" {
 			w.Write([]byte("Empty Authorization header"))
-
 		} else {
-			if reqAuthString == validAuthString {
-				w.Write([]byte(secret))
-			} else {
-				w.Write([]byte("401"))
-			}
+			w.Write([]byte(reqAuthString))
 		}
 	}))
 
@@ -276,10 +175,10 @@ func basicAuthServer(creds mockCreds, secret string, t *testing.T) *httptest.Ser
 }
 
 func basicAuthRequest(url string, creds mockCreds, t *testing.T) string {
-	reqAuthString := b64.StdEncoding.EncodeToString([]byte(creds.UserName + ":" + creds.Password))
+	reqAuthString := "Basic " + b64.StdEncoding.EncodeToString([]byte(creds.UserName+":"+creds.Password))
 
 	// create ClusterSecretStore
-	testStore := &esv1beta1.ClusterSecretStore{
+	basicAuthStore := &esv1beta1.ClusterSecretStore{
 		TypeMeta: metav1.TypeMeta{
 			Kind: "ClusterSecretStore",
 		},
@@ -299,9 +198,31 @@ func basicAuthRequest(url string, creds mockCreds, t *testing.T) string {
 		},
 	}
 
-	// create HTTP client from ClusterSecretStore
-	testProv := &Provider{}
-	client, err := testProv.NewClient(context.Background(), testStore, nil, "testnamespace")
+	return (exerciseGetSecret(basicAuthStore, nil, t))
+	/*
+	   // create HTTP client from ClusterSecretStore
+	   testProv := &Provider{}
+	   client, err := testProv.NewClient(context.Background(), testStore, nil, "testnamespace")
+
+	   	if err != nil {
+	   		t.Errorf("Error creating client: %q", err)
+	   		return "error"
+	   	}
+
+	   // perform request, exercising GetSecret
+	   resp, err := client.GetSecret(context.Background(), esv1beta1.ExternalSecretDataRemoteRef{Key: "dummy"})
+
+	   	if err != nil {
+	   		t.Errorf("Error retrieving secret:%s", err)
+	   	}
+
+	   return string(resp)
+	*/
+}
+
+func exerciseGetSecret(mockStore esv1beta1.GenericStore, mockKubeClient client.Client, t *testing.T) string {
+	mockProvider := &Provider{}
+	client, err := mockProvider.NewClient(context.Background(), mockStore, mockKubeClient, "default")
 	if err != nil {
 		t.Errorf("Error creating client: %q", err)
 		return "error"
